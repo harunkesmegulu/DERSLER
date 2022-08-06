a644d332e53063170b6334b8e45992ceab8d9eca
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-06 21:09:15 +0300

    js

---
+++ 06.08_JS/app.js
@@ -0,0 +1,93 @@
+console.log('app.js is loaded');
+
+
+
+// let firstName = prompt('Enter your name:');
+// alert('firstName :' + firstName);
+// let sonuc = confirm('are you legal?');
+// console.log(sonuc);
+
+// var myNumber1 = 11;
+// console.log(myNumber1);
+// let v2 = 55,
+// console.log(v2)
+
+
+// var myNumber1 = 22;
+
+
+//const
+
+// var myNumber = 3
+// //this is pre es6 style, prefer let or const
+// let num1 = 15;
+// const userName = 'Harun';
+
+let aValue;
+console.log("1. " + typeof aValue);
+//1.underfiened
+aValue = null; // beware: null is a value, not a reference
+console.log("2. " + typeof aValue);
+//2.objesct
+aValue = 42;
+console.log("3. " + typeof aValue);
+//3.number
+aValue = "Clarusway";
+// aValue = 'Clarusway';
+// aValue = `Clarusway`;
+// 👆 3 ways to define a string
+console.log("4. " + typeof aValue);
+//4.string
+aValue = true;
+console.log("5. " + typeof aValue);
+//5.boolen
+aValue = 1234567890123456789012345678901234567890n;
+console.log("6. " + typeof aValue);
+//6.bigint
+aValue = Symbol("Clarusway");
+console.log("7. " + typeof aValue);
+//7.symbol
+aValue = { firstName: "John", lastName: "Doe" };
+console.log("8. " + typeof aValue);
+//8.object
+
+// let age ;
+// console.log(age); // output: "undefined"
+// console.log(userName); // userName is not defined
+
+// let age2 = null;
+
+let pi = 3.14;
+let x = 7;
+console.log(x);
+console.log(pi);
+console.log( 7.3e9 );
+
+// window.location = `https://${0.1+0.2}.com`;
+
+let myNumber1 = 0x5AD;
+console.log(myNumber1);
+// myNumber equals to 1453 in decimal
+
+// let num1 = 015;
+// let num2 = 025;
+// let result = num1 + num2;
+// console.log(`result is ${result}`); // result is 34
+// num1 = 15;
+// num2 = 025;
+// console.log(`result is ${result}`);// result is 36
+// num1 = 015;
+// num2 = 25;
+// console.log(`result is ${result}`);// result is 38
+// num1 = 15;
+// num2 = 25;
+// console.log(`result is ${result}`);// result is 40 isn't it weird?
+// let numOctal = 0o1234567;
+
+// let myNumber = 0b10011101;
+// console.log(myNumber);
+
+""
+''
+``
+
