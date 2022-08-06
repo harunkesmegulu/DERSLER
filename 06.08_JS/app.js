1f29d561eb1337968c55306c736dcc3a87cda91b
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-07 00:08:23 +0300

    js_work

--- 06.08_JS/app.js
+++ 06.08_JS/app.js
@@ -23,50 +23,50 @@ console.log('app.js is loaded');
 // let num1 = 15;
 // const userName = 'Harun';
 
-let aValue;
-console.log("1. " + typeof aValue);
-//1.underfiened
-aValue = null; // beware: null is a value, not a reference
-console.log("2. " + typeof aValue);
-//2.objesct
-aValue = 42;
-console.log("3. " + typeof aValue);
-//3.number
-aValue = "Clarusway";
-// aValue = 'Clarusway';
-// aValue = `Clarusway`;
-// 👆 3 ways to define a string
-console.log("4. " + typeof aValue);
-//4.string
-aValue = true;
-console.log("5. " + typeof aValue);
-//5.boolen
-aValue = 1234567890123456789012345678901234567890n;
-console.log("6. " + typeof aValue);
-//6.bigint
-aValue = Symbol("Clarusway");
-console.log("7. " + typeof aValue);
-//7.symbol
-aValue = { firstName: "John", lastName: "Doe" };
-console.log("8. " + typeof aValue);
-//8.object
+// let aValue;
+// console.log("1. " + typeof aValue);
+// //1.underfiened
+// aValue = null; // beware: null is a value, not a reference
+// console.log("2. " + typeof aValue);
+// //2.objesct
+// aValue = 42;
+// console.log("3. " + typeof aValue);
+// //3.number
+// aValue = "Clarusway";
+// // aValue = 'Clarusway';
+// // aValue = `Clarusway`;
+// // 👆 3 ways to define a string
+// console.log("4. " + typeof aValue);
+// //4.string
+// aValue = true;
+// console.log("5. " + typeof aValue);
+// //5.boolen
+// aValue = 1234567890123456789012345678901234567890n;
+// console.log("6. " + typeof aValue);
+// //6.bigint
+// aValue = Symbol("Clarusway");
+// console.log("7. " + typeof aValue);
+// //7.symbol
+// aValue = { firstName: "John", lastName: "Doe" };
+// console.log("8. " + typeof aValue);
+// //8.object
 
-// let age ;
-// console.log(age); // output: "undefined"
-// console.log(userName); // userName is not defined
+// // let age ;
+// // console.log(age); // output: "undefined"
+// // console.log(userName); // userName is not defined
 
-// let age2 = null;
+// // let age2 = null;
 
-let pi = 3.14;
+// let pi = 3.14;
 let x = 7;
 console.log(x);
-console.log(pi);
-console.log( 7.3e9 );
+// console.log(pi);
+// console.log( 7.3e9 );
 
-// window.location = `https://${0.1+0.2}.com`;
+// // window.location = `https://${0.1+0.2}.com`;
 
-let myNumber1 = 0x5AD;
-console.log(myNumber1);
+// let myNumber1 = 0x5AD;
+// console.log(myNumber1);
 // myNumber equals to 1453 in decimal
 
 // let num1 = 015;
@@ -87,7 +87,20 @@ console.log(myNumber1);
 // let myNumber = 0b10011101;
 // console.log(myNumber);
 
-""
-''
-``
+// ""
+// ''
+// ``
 
+// Boolean (3 > 2);     //return true
+// Boolean (2 < 3);     //return true
+// Boolean (2 > 3);     //return false 
+// Boolean (2 == 3);    //return false 
+
+// Boolean("");
+// Boolean(0);
+// Boolean(-0);
+// Boolean(null);
+// let x;
+// Boolean(x);   //All of these values return false
+
+typeof (5 <= 4)
---
+++ 06.08_ws_js/app.js
@@ -0,0 +1,19 @@
+let x = 10;
+let y = 20;
+x += y;
+console.log(x); 
+y %= x;
+console.log(y);
+
+let text1 = "HARUN ";
+let text2 = "KESMEGULU"
+text1 += text2
+console.log(text1)
+
+console.log(typeof x)
+
+
+let a = 8;
+let b = 9;
+console.log(a <= b)
+
