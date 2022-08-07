e62ffb6b3a28f4d2d7b03a76de57d755c38a31be
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-08 02:00:03 +0300

    08.08 changes

--- 06.08_ws_js/app.js
+++ 06.08_ws_js/app.js
@@ -1,25 +1,58 @@
-let x = 10;
-let y = 20;
-x += y;
-console.log(x); 
-y %= x;
-console.log(y);
+// let x = 10;
+// let y = 20;
+// x += y;
+// console.log(x); 
+// y %= x;
+// console.log(y);
 
-let text1 = "HARUN ";
-let text2 = "KESMEGULU"
-text1 += text2
-console.log(text1)
+// let text1 = "HARUN ";
+// let text2 = "KESMEGULU"
+// text1 += text2
+// console.log(text1)
 
-console.log(typeof x)
+// console.log(typeof x)
 
 
-let a = 8;
-let b = 9;
-console.log(a <= b)
+// let a = 8;
+// let b = 9;
+// console.log(a <= b)
 
-a= 00001000 =8
-a <<= b
-a= 0000100000 =16
+// a= 00001000 =8
+// a <<= b
+// a= 0000100000 =16
 
-a >>= b
-a
+// a >>= b
+// a
+
+// const x = Number.MAX_SAFE_INTEGER;
+// console.log(typeof x, x);
+
+// const y = BigInt(x + 1);
+// console.log(typeof y, y)
+
+// number 9007199254740991
+// bigint 9007199254740992n
+
+// let Sym1 = Symbol("Sym");
+// let Sym2 = Symbol("Sym");
+// console.log(Sym1 === Sym2);  
+
+// false
+
+// const myCar = new Object();
+// myCar.make = 'Ford';
+// myCar.model = 'Mustang';
+// myCar.year = 1969;
+
+// const myCar = {
+//     make: 'Ford',
+//     model: 'Mustang',
+//     year: 1969
+// }; 
+
+// var length = 40; //number
+
+// var lastName = "Clarusway";
+
+// var x = { firstName: "John", lastName: "Clarusway" };
+// console.log(x, "age:", length)
