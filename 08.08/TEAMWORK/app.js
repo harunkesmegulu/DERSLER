af2cc01d9d972a9c0b2dc52fbea3545d955bbe60
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-09 01:25:27 +0300

    active

--- 08.08/TEAMWORK/app.js
+++ 08.08/TEAMWORK/app.js
@@ -1,66 +1,66 @@
-// let x;
-// x=5
-// console.log(x)
+let u;
+u=5
+console.log(u)
 
-// let x1 ="34.00";
-// let x2 = 34;
-// console.log(x1 == x2) //same walue
+let x1 ="34.00";
+let x2 = 34;
+console.log(x1 == x2) //same walue
 
-// console.log(x1 === x2) //same value but different tipe so it is false
+console.log(x1 === x2) //same value but different tipe so it is false
 ///////////////////////////
-// let y = 123e5;
-// console.log(y);
-// let x = 123e-5;
-// console.log(x);
+let y = 123e5;
+console.log(y);
+let x = 123e-5;
+console.log(x);
 
 let a = 5;
 let b = 6;
 let c = 9;
 
-// console.log(a=c);
+console.log(a=c);
 
-// const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
-// console.log(person.firstName)
+const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log(person.firstName)
 
-// const cars = ["Saab", "Volvo", "BMW"];
-// console.log(cars[2]);
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars[2]);
 // ///////////////////////////////
 
-// console.log(typeof 3);
-// console.log(typeof (3+4));
-// console.log(typeof "person");
+console.log(typeof 3);
+console.log(typeof (3+4));
+console.log(typeof "person");
 //////////////////////////////////
 
 function myFunction(a, b) {
     return a * b;             // Function returns the product of a and b
 }
-// console.log( myFunction(3,4) );  
+console.log( myFunction(3,4) );  
 
 let w = myFunction(4,3)
 console.log(w)
 /////////////////////////////////
 
-// function toCelsius(fahrenheit) {
-//     return (5/9) * (fahrenheit-32);
-//   }
+function toCelsius(fahrenheit) {
+    return (5/9) * (fahrenheit-32);
+  }
 //   ////////////////////////////
 // This is importand.
-// let d = toCelsius(77);
-// console.log(d)
-// console.log( "The temperature is " + d + " Celsius");
+let d = toCelsius(77);
+console.log(d)
+console.log( "The temperature is " + d + " Celsius");
 
-// let text = "The temperature is " + d + " Celsius";
-// console.log(text)
+let text = "The temperature is " + d + " Celsius";
+console.log(text)
 // ////////////////////////
 
 
 // code here can NOT use carName
 
-// function myFunction() {
-//     let carName = "WV";
-//     console.log(carName);
-//     code here CAN use carName
-//   }
-//   console.log(carNAME);
+function myFunction() {
+    let carName = "WV";
+    console.log(carName);
+    // code here CAN use carName
+  }
+  console.log(carNAME);
 //   code here can NOT use carName
 
