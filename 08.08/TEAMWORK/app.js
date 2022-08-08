d6a5f3b62ac7f1f763175dfc5bf5d4bcc6952980
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-09 01:20:20 +0300

    working_data_tipe_and_functions

---
+++ 08.08/TEAMWORK/app.js
@@ -0,0 +1,66 @@
+// let x;
+// x=5
+// console.log(x)
+
+// let x1 ="34.00";
+// let x2 = 34;
+// console.log(x1 == x2) //same walue
+
+// console.log(x1 === x2) //same value but different tipe so it is false
+///////////////////////////
+// let y = 123e5;
+// console.log(y);
+// let x = 123e-5;
+// console.log(x);
+
+let a = 5;
+let b = 6;
+let c = 9;
+
+// console.log(a=c);
+
+// const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// console.log(person.firstName)
+
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(cars[2]);
+// ///////////////////////////////
+
+// console.log(typeof 3);
+// console.log(typeof (3+4));
+// console.log(typeof "person");
+//////////////////////////////////
+
+function myFunction(a, b) {
+    return a * b;             // Function returns the product of a and b
+}
+// console.log( myFunction(3,4) );  
+
+let w = myFunction(4,3)
+console.log(w)
+/////////////////////////////////
+
+// function toCelsius(fahrenheit) {
+//     return (5/9) * (fahrenheit-32);
+//   }
+//   ////////////////////////////
+// This is importand.
+// let d = toCelsius(77);
+// console.log(d)
+// console.log( "The temperature is " + d + " Celsius");
+
+// let text = "The temperature is " + d + " Celsius";
+// console.log(text)
+// ////////////////////////
+
+
+// code here can NOT use carName
+
+// function myFunction() {
+//     let carName = "WV";
+//     console.log(carName);
+//     code here CAN use carName
+//   }
+//   console.log(carNAME);
+//   code here can NOT use carName
+
