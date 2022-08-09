62786f9d1d667402265dbc6901cefae2e4b874e1
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-09 20:46:47 +0300

    data_typ

---
+++ 09.08_js_data_tipe/app.js
@@ -0,0 +1,61 @@
+//lops
+console.log("DATA_TYPES");
+
+var a =5;
+console.log(a);
+
+{
+    console.log(a); 
+    
+}
+console.log(a);
+
+let b = 6;
+console.log(b);
+{
+   
+    console.log(b)
+}
+
+console.log(b)
+////////////////////////////////
+var c = 5;
+console.log(c);
+
+{
+    console.log(c); 
+    var c = 2;
+}
+console.log(c);
+
+
+let d = 6;
+console.log(d);
+{
+   let d = 3;
+    console.log(d);
+}
+
+console.log(d);
+
+/////////////////////////under this line is important
+
+let e = 6;
+console.log(e);
+{
+   e = 3;
+    console.log(e);
+}
+
+console.log(e);
+//////////////////
+
+console.log(typeof 0);
+console.log(typeof 3.14);
+console.log(typeof 'hello');
+console.log(typeof (8 < 10));
+console.log( 8 > 10);
+console.log(typeof '');
+
+let f = prompt("bir değer giriniz");
+console.log(f, typeof2 f)
