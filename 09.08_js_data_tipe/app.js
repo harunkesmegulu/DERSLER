bcbab2303fa977f9e85e8549f128e1ab42b0466b
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-09 22:19:36 +0300

    types

--- 09.08_js_data_tipe/app.js
+++ 09.08_js_data_tipe/app.js
@@ -57,5 +57,52 @@ console.log(typeof (8 < 10));
 console.log( 8 > 10);
 console.log(typeof '');
 
-let f = prompt("bir değer giriniz");
-console.log(f, typeof2 f)
+// let f = prompt("bir değer giriniz");
+// console.log(f, typeof f)
+// let g = +prompt("bir değer giriniz");
+// console.log( typeof g)
+// console.log( typeof typeof g)
+////////////////////////////
+// string olan sayı ifadesini
+// başına + ekleyip number tipine
+// parseInt(stringVar) ile integer number a
+// parseFloat(stringVar) ile kesirli sayı
+// elde ederiz
+/////////////////////////////////////////////
+
+// console.log(myAge);
+// // var myAge = 40;   /////////undefined 
+// // let myAge = 40;  ///// cannot access before initialization
+// console.log(myAge)
+
+// let r;
+// do {
+//     r =prompt("enter a number:");
+
+// } while (isNaN(r));
+// console.log(r, r*r);
+///////////////////////////////
+// console.log("12" == 12);
+// console.log("12" === 12);
+
+/////////////////////////////////
+
+let g = 12345678912345
+let h = 123456789123456789  //out of integer limit
+let j = 0.1234567891234567
+let k = 0.12345678912345678
+
+console.log(g);
+console.log(h);
+console.log(j);
+console.log(k);
+
+console.log(0.1+0.2) ////// 0.30000000000000004
+let l = 0.1 + 0.2
+console.log(+l.toFixed(2))
+
+///////////////////////////
+
+let z = 1_000_000_000
+console.log(z)
+////////////////////////
