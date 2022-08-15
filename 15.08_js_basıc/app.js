c247d401b60631714dac39c80ffde24c8cdda61f
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-15 22:36:41 +0300

    second

--- 15.08_js_basıc/app.js
+++ 15.08_js_basıc/app.js
@@ -6,31 +6,48 @@
 
 //**************** IF-ELSE *******************
 
-console.log("****** CONDITIONS *******");
+// console.log("****** CONDITIONS *******");
 
-const number = prompt("Enter Number3")
-if(number>0){
-    console.log(`${number} positif`);
+// const number = prompt("Enter Number3")
+// if(number>0){
+//     console.log(`${number} positif`);
 
-}else if(number == 0){
-    console.log(`${number} zero`);
-}else{
-    console.log(`${number} negativ`);
+// }else if(number == 0){
+//     console.log(`${number} zero`);
+// }else{
+//     console.log(`${number} negativ`);
+// }
+// console.log(number, typeof number);
+
+
+// //? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
+// //? yazdiriniz.
+
+// const number1 = +prompt ("Enter Number one");
+// const number2 = +prompt ("Enter Number one");
+// const number3 = +prompt ("Enter Number one");
+// if(number1>=number2 && number1>= number3){
+//     console.log(`${number1} biggest one`);
+// }else if (number2>=numb2
+//     er1 &&  number2>=number3){
+//     console.log(`${number2} biggest one`);
+// }else{
+//     console.log(`${number3}biggest one3`);
+// }
+
+
+const n1 = +prompt ("Enter Number one");
+const n2 = +prompt ("Enter Number one");
+const n3 = +prompt ("Enter Number one");
+let biggest = n1
+if(n2>=biggest ){
+    biggest =n2;
 }
-console.log(number, typeof number);
+if (n3>= biggest){
+    biggest = n3
+}4
+    console.log(`${biggest} biggest one`);
 
 
-//? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
-//? yazdiriniz.
 
-const number1 = +prompt ("Enter Number one");
-const number2 = +prompt ("Enter Number one");
-const number3 = +prompt ("Enter Number one");
-if(number1>=number2 && number1>= number3){
-    console.log(`${number1} biggest one`);
-}else if (number2>=numb2
-    er1 &&  number2>=number3){
-    console.log(`${number2} biggest one`);
-}else{
-    console.log(`${number3}biggest one3`);
-}
+    
