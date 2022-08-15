48f399f6eda0c736867d01ca9cba19edb79abf10
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-15 21:48:05 +0300

    if_if-else-else

---
+++ 15.08_js_basıc/app.js
@@ -0,0 +1,20 @@
+//?condition
+
+// * ===============================================
+// *                  KARAR YAPILARI
+// * ===============================================
+
+//**************** IF-ELSE *******************
+
+console.log("****** CONDITIONS *******");
+
+const number = prompt("Enter Number3")
+if(number>0){
+    console.log(`${number} positif`);
+
+}else if(number == 0){
+    console.log(`${number} zero`);
+}else{
+    console.log(`${number} negativ`);
+}
+console.log(number, typeof number);
