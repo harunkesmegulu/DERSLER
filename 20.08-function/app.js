07d4cf6fa25607221092e50071c1f9f202945927
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-20 15:26:11 +0300

    functıons

--- 20.08-function/app.js
+++ 20.08-function/app.js
@@ -81,11 +81,124 @@ tekCift(2);
 
 //* ORNEK:
 //************************************************/
-function tekCift(sayi3) {
-  return sayi3 % 2 ? "tektir" : "ciftir";
-}
-const sayi3 = +prompt("Bir sayı giriniz");
-console.log(tekCift(sayi3));
+//
+//* ORNEK:
+//************************************************3. function expression/
 
-const n = +prompt("Bir sayı giriniz");
-console.log(tekCift(n));
+// // Function expression
+// const tekMi = function (x) {
+//   return x % 2 ? `${sayi} tektir` : `${sayi} cifttir`;
+// };
+// // const sayı = Number(prompt("Sayıyı giriniz:"));
+// console.log(`tekMi(5)`);
+
+const buyukBul = function (n1, n2, n3 = -Number.MAX_VALUE) {
+  let enBuyuk;
+  if (n1 >= n2 && n1 >= n3) {
+    enBuyuk = n1;
+  } else if (n2 >= n1 && n2 >= n3) {
+    enBuyuk = n2;
+  } else if (n3 >= n1 && n3 >= n2) {
+    enBuyuk = n3;
+  }
+  return enBuyuk;
+};
+// const n1 = +prompt("Sayi1:");
+// const n2 = +prompt("Sayi2:");
+// const n3 = +prompt("Sayi3:");
+
+// console.log("Girilen sayıların en bğyğğü:", buyukBul(n1, n2, n3));
+
+//* ORNEK:
+//************************************************/
+
+const buyukBul2 = function (n1 = 1, n2, n3 = -Number.MAX_VALUE) {
+  let enBuyuk;
+  if (n1 >= n2 && n1 >= n3) {
+    enBuyuk = n1;
+  } else if (n2 >= n1 && n2 >= n3) {
+    enBuyuk = n2;
+  } else if (n3 >= n1 && n3 >= n2) {
+    enBuyuk = n3;
+  }
+  return enBuyuk;
+};
+
+console.log("Girilen sayıların en bğyğğü:", buyukBul2(-8, -9));
+
+//**************************************************/
+const topla = function (s1, s2) {
+  return s1 + s2;
+};
+
+const cikar = function (s1, s2) {
+  return s1 - s2;
+};
+const bol = function (s1, s2) {
+  return s1 / s2;
+};
+const carp = function (s1, s2) {
+  return s1 * s2;
+};
+
+const hesapla = function (s1, s2, islem) {
+  let sonuc = 0;
+  switch (islem) {
+    case "+":
+      sonuc = topla(s1, s2);
+      break;
+    case "-":
+      sonuc = cikar(s1, s2);
+      break;
+    case "/":
+      sonuc = bol(s1, s2);
+      break;
+    case "*":
+      sonuc = carp(s1, s2);
+      break;
+    default:
+      break;
+  }
+  return sonuc;
+};
+
+console.log(hesapla(3, 5, "+"));
+console.log(hesapla(4, 3, "/"));
+//**************************************************/
+
+const topla2 = function (s1, s2) {
+  return s1 + s2;
+};
+
+const cikar2 = function (s1, s2) {
+  return s1 - s2;
+};
+const bol2 = function (s1, s2) {
+  return s1 / s2;
+};
+const carp2 = function (s1, s2) {
+  return s1 * s2;
+};
+
+const hesapla2 = function (s1, s2, islem) {
+  let sonuc = 0;
+  switch (islem) {
+    case "+":
+      return topla2(s1, s2);
+
+    case "-":
+      return cikar2(s1, s2);
+
+    case "/":
+      return bol2(s1, s2);
+
+    case "*":
+      return carp2(s1, s2);
+
+    default:
+      break;
+  }
+};
+
+console.log(hesapla2(3, 5, "+"));
+console.log(hesapla2(4, 3, "/"));
