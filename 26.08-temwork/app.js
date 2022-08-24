3e63e3da2866dbf57eed4c159ea097ea7c969840
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-25 02:23:39 +0300

    25.08

---
+++ 26.08-temwork/app.js
@@ -0,0 +1,34 @@
+// var a = ["dog", "lion", "hen"];
+// console.log(a.length);
+// a[1] = "1";
+// console.log(a.length);
+// a[105] = "horse";
+// console.log(a.length);
+// a[101] = "horse";
+// console.log(a.length);
+// a[5] = "dog";
+// console.log(a.length);
+
+//***************************Reverse String********************************************** */
+// let a = prompt("Ters çevrilmesini istediğin kelimeyi yaz!");
+// const a = "hello";
+// // const spl = a.split("");
+// for (let i = spl.length - 1; i >= 0; i--) {
+//   console.log(spl[i]);
+// }
+//****************************************************************** */
+// let sonuc = a.split("").reverse().join("");
+// console.log(sonuc);
+//*************************************************************************** */
+// const a1 = "hello";
+// let metin = [];
+// for (let i = a1.length - 1; i >= 0; i--) {
+//   metin.push(a1[i]);
+// }
+// console.log(metin.join(""));
+
+//******************************Reverse Words**************************************** */
+// let a = "Ters çevrilmesini istediğin kelimeyi yaz!";
+// let sonuc = a.split(" ").reverse().join(" ");
+// console.log(sonuc);
+//***************************CSS Grid********************************** */
