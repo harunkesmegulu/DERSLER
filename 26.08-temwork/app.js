34da84d4facbfe81f9e181c0422fba927a933f12
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-25 03:08:44 +0300

    last

--- 26.08-temwork/app.js
+++ 26.08-temwork/app.js
@@ -1,25 +1,26 @@
 // var a = ["dog", "lion", "hen"];
 // console.log(a.length);
 // a[1] = "1";
+// console.log(a[1]);
+// console.log(a.length);
+// a[100] = "horse";
 // console.log(a.length);
 // a[105] = "horse";
 // console.log(a.length);
-// a[101] = "horse";
-// console.log(a.length);
-// a[5] = "dog";
+// a[25] = "dog";
 // console.log(a.length);
 
 //***************************Reverse String********************************************** */
-// let a = prompt("Ters çevrilmesini istediğin kelimeyi yaz!");
+// // let a = prompt("Ters çevrilmesini istediğin kelimeyi yaz!");
 // const a = "hello";
-// // const spl = a.split("");
+// const spl = a.split("");
 // for (let i = spl.length - 1; i >= 0; i--) {
 //   console.log(spl[i]);
 // }
-//****************************************************************** */
+//*********************************3********************************* */
 // let sonuc = a.split("").reverse().join("");
 // console.log(sonuc);
-//*************************************************************************** */
+//*********************************2****************************************** */
 // const a1 = "hello";
 // let metin = [];
 // for (let i = a1.length - 1; i >= 0; i--) {
