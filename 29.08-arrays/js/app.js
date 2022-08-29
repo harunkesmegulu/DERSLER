6c7dc0337cd70de266f72a60cf8e06c6567a70bc
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-29 20:41:08 +0300

    first

---
+++ 29.08-arrays/js/app.js
@@ -0,0 +1,12 @@
+// const arr = ["ahmet", "mehmet", "fatih"];
+
+// const language = new Array("C++", "Go");
+
+const a = [3];
+console.log(a.length);
+
+const numbers1 = new Array(10);
+console.log(numbers1.length);
+
+const veriler = Array.of(1, 2, 3);
+console.log(veriler);
