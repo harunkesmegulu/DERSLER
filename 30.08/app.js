428e8d447c8bf3bf8068b233585f812ec46da2fd
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-31 00:00:30 +0300

    second

---
+++ 30.08/app.js
@@ -0,0 +1,18 @@
+// const username = "KaanCansu_30";
+// console.log(username.split(""));
+// let splitName = username.split("");
+// if (25 >= splitName.length && splitName.length >= 4) {
+//   if (splitName[0].includes([/^([a-zA-Z])$/])) console.log(true);
+// } else {
+//   console.log(false);
+// }
+
+
+const username = "_30";
+console.log(username.split(""));
+let splitName = username.split("");
+if (25 >= splitName.length && splitName.length >= 4) {
+  console.log(true);
+} else if (splitName[0].includes([/^([a-zA-Z])$/])) {
+  console.log(true);
+} else console.log(false);
