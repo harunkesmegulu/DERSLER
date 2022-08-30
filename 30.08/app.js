6294ebf4a7526a445030305a0f8fec452d37e51b
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-08-31 00:04:58 +0300

    last

--- 30.08/app.js
+++ 30.08/app.js
@@ -8,11 +8,12 @@
 // }
 
 
-const username = "_30";
-console.log(username.split(""));
-let splitName = username.split("");
-if (25 >= splitName.length && splitName.length >= 4) {
-  console.log(true);
-} else if (splitName[0].includes([/^([a-zA-Z])$/])) {
-  console.log(true);
-} else console.log(false);
+// const username = "_30";
+// console.log(username.split(""));
+// let splitName = username.split("");
+// if (25 >= splitName.length && splitName.length >= 4) {
+//   console.log(true);
+// } else if (splitName[0].includes([/^([a-zA-Z])$/])) {
+//   console.log(true);
+// } else console.log(false);
+
