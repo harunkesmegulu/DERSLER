0ac630415849a364e6a28d61868136aa1925b41d
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-09-03 00:11:31 +0300

    ver

--- 02.09CODE-CHALLENGE/teamwork/app.js
+++ 02.09CODE-CHALLENGE/teamwork/app.js
@@ -1,7 +1,12 @@
-let numbers = [10,15,20,30,40];
-// numbers.forEach((num)=>{
-// console.log(num*num)
-// })
-const newArray = numbers.map(num => num*num);
+// let numbers = [10,15,20,30,40];
+// // numbers.forEach((num)=>{
+// // console.log(num*num)
+// // })
+// const newArray = numbers.map(num => num*num);
+// console.log(newArray);
+
+let avengers = [`iron man`,`captain america`, `Black widow]`,`hulk`];
+const newArray = avengers.map(avenger => avenger.toUpperCase());
 console.log(newArray);
 
+const newArray = avengers.slice
