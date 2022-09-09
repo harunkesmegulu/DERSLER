bd306840113e7dc9f3a372fab85d2d87359c4997
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-09-09 21:51:17 +0300

    TO-DO

--- 09.09/js.js
+++ 09.09/js.js
@@ -11,3 +11,20 @@ myCar.age(2030)
 for(let key in myCar){
     console.log(key)
 }
+//todo //BİLİNMESİ GEREKEN 15 JS ARRAY METODU//
+​
+01; find();
+02; includes();
+03; filter();
+04; concat();
+05; push();
+06; join();
+07; sort();
+08; slice();
+09; splice();
+10; every();
+11; some();
+12; reduce();
+13; findIndex();
+14; entries();
+15; map();
