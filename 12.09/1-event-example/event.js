bfb51761264acd876468583451d31347549bb5a0
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-09-12 22:09:02 +0300

    correction

---
+++ 12.09/1-event-example/event.js
@@ -0,0 +1,148 @@
+const h1 = document.querySelector("#header h1");
+console.log(h1);
+document.querySelector("#header h1");
+
+h1.onmouseover = function (){
+    h1.style.color = "red";
+    h1.style.fontWeight ="300";
+};
+h1.onmouseout = () => {
+    h1.style.color = "black";
+    h1.style.fontWeight ="900";
+}
+h1.addEventListener("click", () => {
+    alert("H1 pressed");
+});
+
+//*********example?/
+
+document.getElementById("btn").addEventListener(`click`, function(){
+    const input = document.querySelector("#input");
+    // if(!input.value) {
+    //     alert("Please enter an item");
+    // } else {
+    //     alert(`${input.value} entered`);
+    // }
+
+
+// !input.value
+// ? alert("Please enter an item")
+// ! alert(`${input.value} entered`);
+// input.value = "";
+
+})
+
+
+//* EXAMPLE-3
+//* -------------------------------------------------
+const list = document.querySelectorAll(".list"); //?nodelist
+
+list.forEach((li) => {
+  li.style.transition = "all 0.8s";
+  li.style.lineHeight = "2rem";
+
+  li.onmouseover = () => {
+    li.style.fontSize = "2rem";
+    li.style.transform = "translateX(10px)";
+  };
+
+  li.onmouseout = () => {
+    li.style.fontSize = "1rem";
+    li.style.transform = "translateX(-10px)";
+  };
+});
+
+// const print = () => {
+//   console.log("Starting");
+// };
+
+
+//* EXAMPLE-3
+
+window.on//* ======================================================
+//*                        EVENTS
+//* ======================================================
+
+console.log("****** EVENTS *******");
+
+//* EXAMPLE-1 (Mouse Over, Mouse Out )
+//* -------------------------------------------------------
+
+const h1 = document.querySelector("#header h1");
+
+//? 3 .method
+h1.onmouseover = function () {
+  h1.style.color = "red";
+  h1.style.fontWeight = "300";
+  console.log("onmouseover");
+};
+
+h1.onmouseout = () => {
+  h1.style.color = "black";
+  h1.style.fontWeight = "900";
+  console.log("onmouseout");
+};
+
+//? 4.method (addEventListener)
+h1.addEventListener("click", () => {
+  alert("H1 pressed");
+});
+
+//* EXAMPLE-2 (addEventListener())
+//* -------------------------------------------------
+
+document.getElementById("btn").addEventListener("click", function () {
+  const input = document.querySelector("#input");
+  // if (!input.value) {
+  //   alert("Please enter an item");
+  // } else {
+  //   alert(`${input.value} entered`);
+  // }
+
+  !input.value
+    ? alert("Please enter an item")
+    : alert(`${input.value} entered`);
+
+  input.value = "";
+  document.querySelector("#input").focus();
+});
+
+//* EXAMPLE-3
+//* -------------------------------------------------
+const list = document.querySelectorAll(".list"); //?nodelist
+
+list.forEach((li) => {
+  li.style.transition = "all 0.8s";
+  li.style.lineHeight = "2rem";
+
+  li.onmouseover = () => {
+    li.style.fontSize = "2rem";
+    li.style.transform = "translateX(10px)";
+  };
+
+  li.onmouseout = () => {
+    li.style.fontSize = "1rem";
+    li.style.transform = "translateX(-10px)";
+  };
+});
+
+//* EXAMPLE-4 (onload)
+//* -------------------------------------------------
+window.onload = function () {
+  document.querySelector("#input").focus();
+};
+
+const print = () => {
+  console.log("Starting");
+};
+
+print();
+load = function (){
+    document.querySelector("#input").focus();
+};
+const print = () => {
+    console.log("starting");
+}
+
+
+print();
