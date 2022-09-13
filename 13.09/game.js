f10329da12713631550da0ada811304e918d5cb7
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-09-13 22:12:32 +0300

    input

--- 13.09/game.js
+++ 13.09/game.js
@@ -2,3 +2,39 @@
 //*  GUESS MY NUMBER
 //*  Game Logic
 //*======================================
+//** 1-100 arası sayı tut */
+
+
+const randomNumber = Math.floor(Math.random() * 100) +1;
+console.log(randomNumber);
+
+//** check butonu basıldığında girilen sayıyı kontrel et */
+document. querySelector(".check-btn").addEventListener("click", () => {
+    const guessInput = Number(document.querySelector(".guess-input").value);
+    // console.log(guessInput)
+    const msg = document.querySelector(".msg");
+
+
+
+    //** eğer sayı girilmediyse kullanıcıya uyarı ver */
+    
+if (!guessInput){
+    msg.innerText = "Please enter a number";
+    }else if (randomNumber === guessInput) {
+        msg.innerText = `Congragulation You Win 😍 <i class="fa-regular fa-face-awesome"></i> `;
+        body.className = "bg-success";
+    }
+
+
+
+})
+
+  
+
+//** eğer rasgele == input.value (tebrikler) background rengi yeşil....eger top score > topScore topscore = score secret-number visible*/
+
+//** değilse eğer score >0 sa score = score -1 */
+//** eğer rasgele tutulan sayı input value dan küçükse AZALT demek gerek değilse artır...*/
+
+
+//** 1-100 arası sayı tut */
