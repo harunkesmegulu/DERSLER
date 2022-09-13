961e4c9dd4644ec3c9c1e709fcb73651009cc9da
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-09-13 23:07:10 +0300

    final

--- 13.09/game.js
+++ 13.09/game.js
@@ -2,42 +2,46 @@
 //*  GUESS MY NUMBER
 //*  Game Logic
 //*======================================
-//** 1-100 arası sayı tut */
 
-
-const randomNumber = Math.floor(Math.random() * 100) +1;
+//? 1-100 arasinda rasgele bir sayi tut.
+let randomNumber = Math.round(Math.random() * 100);
 console.log(randomNumber);
-
-// let randomNumber = Math.round(Math.random() * 100);
-// console.log(randomNumber); //***bu şekide de olur */
 
 //? Variables
 let score = 10;
-let topScore = 0;
+// let topScore = 0;
 
-//** check butonu basıldığında girilen sayıyı kontrel et */
+//? localStorage'de topScore adiyla bir degisken olustur.
+let topScore = localStorage.getItem("topScore") || 0;
 
+//? DOM'daki top-score degerini localStorage'den okuyarak guncelle.
+document.querySelector(".top-score").textContent = topScore;
 
+//* CheckBtn basildiginda kontrolleri yap
 document.querySelector(".check-btn").addEventListener("click", () => {
-    const guessInput = Number(document.querySelector(".guess-input").value);
-    const msg = document.querySelector(".msg");
-    const body = document.querySelector("body");
-  
-    //? eger input girilmediyse Kullaniciya uyari ver.
-    if (!guessInput) {
-      msg.innerText = "Please enter a number";
-      //! eger rasgele == input.value
-    } else if (randomNumber === guessInput) {
-      msg.innerHTML = `Congratulations You Win ✨ <i class="fa-solid fa-face-grin-hearts fa-2x"></i> `;
-      body.className = "bg-success";
-      document.querySelector(".check-btn").disabled = true;
-      if (score > topScore) {
-        topScore = score;
-        document.querySelector(".top-score").textContent = topScore;
-      }
-      document.querySelector(".secret-number").textContent = randomNumber;
+  const guessInput = Number(document.querySelector(".guess-input").value);
+  const msg = document.querySelector(".msg");
+  const body = document.querySelector("body");
 
-        //! eger rasgele!= input.value
+  //? eger input girilmediyse Kullaniciya uyari ver.
+  if (!guessInput) {
+    msg.innerText = "Please enter a number";
+    //! eger rasgele == input.value
+  } else if (randomNumber === guessInput) {
+    msg.innerHTML = `Congrats You Win <i class="fa-solid fa-face-grin-hearts fa-2x"></i> `;
+    body.className = "bg-success";
+    document.querySelector(".check-btn").disabled = true;
+    if (score > topScore) {
+      // topScore = score;
+
+      //? localStorage'deki topScore degiskenini guncelle
+      localStorage.setItem("topScore", score);
+      //? DOM'daki top-score degerini guncelle
+      document.querySelector(".top-score").textContent = score;
+    }
+    document.querySelector(".secret-number").textContent = randomNumber;
+
+    //! eger rasgele!= input.value
   } else {
     score--;
     if (score > 0) {
@@ -53,24 +57,26 @@ document.querySelector(".check-btn").addEventListener("click", () => {
 
     document.querySelector(".score").textContent = score;
   }
-
-
-
 });
 
 //* again basildiginda oyunu baslangic dgerlerin kur
 document.querySelector(".again-btn").addEventListener("click", () => {
-    score = 10;
-    document.querySelector(".score").textContent = score;
-    randomNumber = Math.round(Math.random() * 100);
-    document.querySelector(".secret-number").textContent = "?";
-    console.log(randomNumber);
-    document.querySelector(".check-btn").disabled = false;
-    document.querySelector("body").classList.remove("bg-success", "bg-danger");
-    document.querySelector(".guess-input").value = "";
-    document.querySelector(".msg").innerText = `Starting..`;
-  });
-  
+  score = 10;
+  document.querySelector(".score").textContent = score;
+  randomNumber = Math.round(Math.random() * 100);
+  document.querySelector(".secret-number").textContent = "?";
+  console.log(randomNumber);
+  document.querySelector(".check-btn").disabled = false;
+  document.querySelector("body").classList.remove("bg-success", "bg-danger");
+  document.querySelector(".guess-input").value = "";
+  document.querySelector(".msg").innerText = `Starting..`;
+});
+
+document.querySelector(".guess-input").addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    document.querySelector(".check-btn").click();
+  }
+});
 
 //** eğer rasgele == input.value (tebrikler) background rengi yeşil....eger top score > topScore topscore = score secret-number visible*/
 
