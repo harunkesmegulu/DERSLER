8e37010e6bdb9b6a1b43e6757b3761b25515b78c
kesme <109390147+harunkesmegulu@users.noreply.github.com>
2022-10-04 00:50:29 +0300

    initial

---
+++ 03.10/src/math.test.js
@@ -0,0 +1,14 @@
+//? node module import format
+
+const { 
+    calculateTotal,
+ } =require("./math.js")
+//! es6 module format
+describe('calculateTotal', () => {
+})
+describe('celciusToFahrenheit', () => {
+})
+describe('fahrenheitToCelcius', () => {
+})
+describe('fibonacci', () => {
+})
