5872b91d197820ea9f04029ff1103533cffeb706
harunkesmegulu <kesme1213@gmail.com>
2022-10-12 20:43:08 +0300

    add-buttons

--- 12.10/event/src/components/1-events/Events.jsx
+++ 12.10/event/src/components/1-events/Events.jsx
@@ -8,25 +8,42 @@
 //? bir sarmalayici (Wrapper) arabirimle ortulmesi ile olusur. Bu sayede,
 //? React ortaminda kullanilan event'larin bilindik tarayicilarda
 //? sorunsuz calismasini saglanir.
+
 //? Ayrinti icin : https://reactjs.org/docs/events.html
 
 const Events = () => {
-    const handleClick = () =>{
-        alert("Btn Clicked")
-    }
-
-
-
-
+    const handleClick = () => {
+      alert("Btn Clicked");
+    };
+    const handleClear = (msg) => {
+      alert(msg);
+    };
+  
+    const handleChange = (e) => {
+      console.log(e.target);
+    };
+  
     return (
       <div className="container text-center mt-4">
         <button onClick={handleClick} className="btn btn-success">
-            Click
+          Click
         </button>
-        <button onClick=></button>
-
+  
+        <button
+          onClick={() => handleClear("Clear Btn Clicked")}
+          className="btn btn-dark"
+        >
+          Clear
+        </button>
+        <button onClick={handleChange} className="btn btn-danger">
+          Change
+        </button>
+        {/* <button onClick={(e) => handleChange(e)} className="btn btn-danger">
+          Change
+        </button> */}
       </div>
     );
   };
   
   export default Events;
+  
