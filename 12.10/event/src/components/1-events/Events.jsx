15b37832a0fdb0a58eb39189a6ff39cbed6b5b68
harunkesmegulu <kesme1213@gmail.com>
2022-10-12 21:30:21 +0300

    state

--- 12.10/event/src/components/1-events/Events.jsx
+++ 12.10/event/src/components/1-events/Events.jsx
@@ -12,6 +12,8 @@
 //? Ayrinti icin : https://reactjs.org/docs/events.html
 
 const Events = () => {
+    let message = "EVENT BASED PROGRAM";
+  
     const handleClick = () => {
       alert("Btn Clicked");
     };
@@ -21,20 +23,27 @@ const Events = () => {
   
     const handleChange = (e) => {
       console.log(e.target);
+      message = "REACT";
+      console.log(message);
     };
   
     return (
       <div className="container text-center mt-4">
+        <h1>{message}</h1>
         <button onClick={handleClick} className="btn btn-success">
           Click
         </button>
   
+        {/* Eger bir event fonksiyonunun paremetresi olmasi gerekiyorsa
+        bu fonksiyon bir arrow fonks. tarafindan  cagrilmalidir. Aksi
+        takdirde event fonksiyonu event gerceklesmeden cagirilir */}
         <button
           onClick={() => handleClear("Clear Btn Clicked")}
           className="btn btn-dark"
         >
           Clear
         </button>
+  
         <button onClick={handleChange} className="btn btn-danger">
           Change
         </button>
@@ -46,4 +55,19 @@ const Events = () => {
   };
   
   export default Events;
+  
+  //! message console'da guncellendigini ancak DOM'da guncellenmedigini gorduk.
+  //* Bunun sebebi REACT'in aksi belirtilmedigi surece elementleri
+  //* static olarak kabul etmesinden kaynaklanir.
+  
+  //* React bunu DOM islemlerini minimize etmek icin yapmaktadir.
+  //* REACT'a hangi elementleri interaktif oldugu belirtilmelidir.
+  //! React'a elementlerin interaktif oldugunu belirtmek icin state'ler kullanilir.
+  //! State, elementlerin degisiklik durumlarini tutan nesnelerdir.
+  //? ReactJs'de state'leri kullanmak icin  2 ayri Component yapisi bulunmaktadir.
+  //? 1. Statefull Classes (Class Components).
+  //? 2. Hooks (Functional Components).
+  
+  //?Biz su ana kadar uygulamalarimizda Fonksiyonel Component'leri kullandik.
+  //? Yaygin kullanim Fonksiyonel Component'lerdir.
   
