330e3bdb12f655b7e6618fd4a5cf838e8e2fce7e
harunkesmegulu <kesme1213@gmail.com>
2022-10-13 23:45:23 +0300

    addboostrap-transform-jsx

---
+++ 13.10/forms/src/components/1-forms/Form.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <>
+    <div className="form-floating mb-3">
+      <input
+        type="email"
+        className="form-control"
+        id="floatingInput"
+        placeholder="name@example.com"
+      />
+      <label htmlFor="floatingInput">Email address</label>
+    </div>
+    <div className="form-floating">
+      <input
+        type="password"
+        className="form-control"
+        id="floatingPassword"
+        placeholder="Password"
+      />
+      <label htmlFor="floatingPassword">Password</label>
+    </div>
+  </>
+  
+  )
+}
+
+export default Form
