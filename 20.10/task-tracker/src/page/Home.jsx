fbc99b991cc1fa462aa2cc8a16ff2cc2cf907ce7
harunkesmegulu <kesme1213@gmail.com>
2022-10-20 21:13:39 +0300

    task

---
+++ 20.10/task-tracker/src/page/Home.jsx
@@ -0,0 +1,43 @@
+import React,{useState,useEffect} from "react";
+import AddTask from "../components/addTask/AddTask";
+import TaskList from "../components/taskList/TaskList";
+import Button from "react-bootstrap/Button"
+import axios from "axios";
+
+const Home = () => {
+  const [isOpen,setIsOpen]=useState(false)
+  const [text,setText]=useState("Show Task Bar")
+  const [task,setTask]= useState([]);
+  const url="https://63516c99dfe45bbd55bfd9a2.mockapi.io/api/tasks"
+
+  const toggle=()=>{
+    setIsOpen(!isOpen);
+    const buttonText=isOpen ? "Show Task Bar" : "Hide Task Bar"
+    setText(buttonText);
+  }
+
+  // CRUD  -READ--
+  const getTask=async()=>{
+    const {data}=await axios(url)
+    setTask(data);
+    console.log(data)
+  }
+
+  useEffect(() => {
+    getTask()
+  },[])
+
+  return (
+    <div>
+      <Button 
+      onClick={()=>{toggle()}}
+      size="lg"
+      variant="danger">{text}</Button>
+      { isOpen && <AddTask/>}
+     
+      <TaskList />
+    </div>
+  );
+};
+
+export default Home;
