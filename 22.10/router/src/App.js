025aa2451f58633a1770e13cccb80f3c9ad23c4e
harunkesmegulu <kesme1213@gmail.com>
2022-10-22 14:36:01 +0300

    router-final

--- 22.10/router/src/App.js
+++ 22.10/router/src/App.js
@@ -4,23 +4,17 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Instructors from "./pages/Instructors";
 import NotFound from "./pages/NotFound";
-import {BrowserRouter, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Nav />
-      <Routes>
-
       <Home />
       <Instructors />
       <Contact />
-
-
       <NotFound />
-      </Routes>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
--- 22.10/router/src/pages/Instructors.jsx
+++ 22.10/router/src/pages/Instructors.jsx
@@ -5,4 +5,4 @@ const Instructors = () => {
     )
   }
   
-  export default Instructors
+  export default Instructors;
