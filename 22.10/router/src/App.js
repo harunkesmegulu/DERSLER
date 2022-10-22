5850557a17a8a5e3eeac97e50686571a71e618c5
harunkesmegulu <kesme1213@gmail.com>
2022-10-22 11:47:01 +0300

    pages

--- 22.10/router/src/App.js
+++ 22.10/router/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Instructors from "./pages/Instructors";
+import NotFound from "./pages/NotFound";
+import {BrowserRouter, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        
+      <Home />
+      <Instructors />
+      <Contact />
+
+
+      <NotFound />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
