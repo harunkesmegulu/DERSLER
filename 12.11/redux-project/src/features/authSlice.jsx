c5758b9fd380f6f4f839d1bf3c57f71ec1f7de27
harunkesmegulu <kesme1213@gmail.com>
2022-11-12 12:10:44 +0300

    authSlice

--- 12.11/redux-project/src/features/authSlice.jsx
+++ 12.11/redux-project/src/features/authSlice.jsx
@@ -11,7 +11,15 @@ const authSlice = createSlice ({
         setUser:(state, {payload}) => {
             state.user = payload;
         },
-        clearUser: () => {},
+        clearUser: (state) => {
+            state.user = null;
+        },
 
     },
 });
+
+
+export const { setUser, clearUser } = authSlice.actions;
+export default authSlice.reducer;
+
+// reducer ve actions dahili olarak authSlice içinde oluşturuluyor.
