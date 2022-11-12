2211d2fbf2ea61d6e7530c2faa3e33581e70aa1e
harunkesmegulu <kesme1213@gmail.com>
2022-11-12 12:00:22 +0300

    createSlice

---
+++ 12.11/redux-project/src/features/authSlice.jsx
@@ -0,0 +1,17 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = {
+    user: null,
+};
+
+const authSlice = createSlice ({
+    name:"auth",
+    initialState,
+    reducers:{
+        setUser:(state, {payload}) => {
+            state.user = payload;
+        },
+        clearUser: () => {},
+
+    },
+});
