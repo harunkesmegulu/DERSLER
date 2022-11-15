a7b567cdfe91294f7488d53d01d29c50b5499899
harunkesmegulu <kesme1213@gmail.com>
2022-11-15 20:32:57 +0300

    initial

---
+++ 12.11/redux-project/src/features/newsSlice.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+
+}
+
+const newsSlice = createSlice({
+  name: second,
+  initialState,
+  reducers: {}
+});
+
+export const {} = newsSlice.actions
+
+export default newsSlice.reducer
