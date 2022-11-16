760369b3aa44fc79a6c2997fe334dcfa67a7c3e6
harunkesmegulu <kesme1213@gmail.com>
2022-11-17 01:31:50 +0300

    BLOCK-APP

---
+++ 17.11/BLOG-APP/src/App.js
@@ -0,0 +1,16 @@
+
+
+import AppRouter from './app-router/AppRouter';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppRouter />
+        
+    </div>
+  );
+}
+
+export default App;
