541eb86c3511d4718ca8babd93a7eaad418d612e
harunkesmegulu <kesme1213@gmail.com>
2022-11-28 20:38:38 +0300

    START

--- 21.11/stock-app/src/pages/Dashboard.jsx
+++ 21.11/stock-app/src/pages/Dashboard.jsx
@@ -31,18 +31,7 @@ function ResponsiveDrawer(props) {
     <div>
       <Toolbar />
       <Divider />
-      <List>
-        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+     
       <Divider />
       <List>
         {['All mail', 'Trash', 'Spam'].map((text, index) => (
