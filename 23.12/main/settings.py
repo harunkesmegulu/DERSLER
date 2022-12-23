715b5ad3635e00eebaadb7ce6b95a4b143268b86
harunkesmegulu <kesme1213@gmail.com>
2022-12-23 23:10:36 +0300

    django_basics

--- 23.12/main/settings.py
+++ 23.12/main/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from decouple import config  
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h@i5oo-q_!81h+*@utom(@l_+i=oc&tvpo%klvfn=)g1tol5nr'
+# SECRET_KEY = 'django-insecure-h@i5oo-q_!81h+*@utom(@l_+i=oc&tvpo%klvfn=)g1tol5nr'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'teamanka',
 ]
 
 MIDDLEWARE = [
---
+++ 23.12/teamanka/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TeamankaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'teamanka'
